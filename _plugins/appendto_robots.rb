95c9eafe5f7c61f9f2969c10e793fb1478de9aeb
RawGaze <peter@rawgaze.com>
2025-02-11 13:41:42 +0100

    Pluggins added for extra images sitemap

---
+++ _plugins/appendto_robots.rb
@@ -0,0 +1,18 @@
+Jekyll::Hooks.register :site, :post_write do |site|
+    robots_path = File.join(site.dest, "robots.txt")
+  
+    if File.exist?(robots_path)
+      content = File.read(robots_path)
+  
+      # Voorkomen dat de regel meerdere keren wordt toegevoegd
+      unless content.include?("Sitemap: #{site.config['url']}/sitemap-images.xml")
+        File.open(robots_path, "a") do |file|
+          file.puts "Sitemap: #{site.config['url']}/sitemap-images.xml"
+        end
+        puts "✅ Plugin appendto_robots.rb zegt: Toegevoegd aan robots.txt: Sitemap: #{site.config['url']}/sitemap-images.xml"
+      end
+    else
+      puts "⚠️ Waarschuwing: robots.txt niet gevonden in _site, niets toegevoegd."
+    end
+  end
+  
